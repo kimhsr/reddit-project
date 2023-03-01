36c770d612947d66d0f9409b8b5671ec12fafc3f
kimhsr <kimhsr@naver.com>
2023-03-02 00:41:29 +0900

    feat: Tailwind CSS

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const index = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default index;
