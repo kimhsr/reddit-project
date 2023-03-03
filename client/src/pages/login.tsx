5349c6def766a8cbd61fa995744d9c63e9c99d5f
kimhsr <kimhsr@naver.com>
2023-03-03 20:48:43 +0900

    feat: 유저 정보 Context

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,6 +3,7 @@ import InputGroup from "../components/InputGroup";
 import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { useAuthDispatch } from "../context/auth";
 
 const Login = () => {
   let router = useRouter();
@@ -11,15 +12,21 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
 
+  const dispatch = useAuthDispatch();
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     try {
-      await axios.post(
+      const res = await axios.post(
         "/auth/login",
         { password, username },
         { withCredentials: true }
       );
+
+      dispatch("LOGIN", res.data?.user);
+
+      router.push("/");
     } catch (error: any) {
       console.log(error);
       setErrors(error.response.data || {});
