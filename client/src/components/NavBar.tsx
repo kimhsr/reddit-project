71340e1fd52c31df29d820d5fd40e325146bbe31
Kim Hyun Sung <kimhsr@naver.com>
2023-03-06 15:29:03 +0900

    feat: 로그아웃 기능 (#16)

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import Link from "next/link";
 import React from "react";
-import { useAuthState } from "../context/auth";
+import { useAuthDispatch, useAuthState } from "../context/auth";
 
 const NavBar: React.FC = () => {
   const { loading, authenticated } = useAuthState();
+  const dispatch = useAuthDispatch();
+  const handleLogout = () => {
+    axios
+      .post("/auth/logout")
+      .then(() => {
+        dispatch("LOGOUT");
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between h-16 px-5 bg-white">
       <span className="text-2xl font-semibold text-gray-400">
@@ -22,7 +35,10 @@ const NavBar: React.FC = () => {
       <div className="flex">
         {!loading &&
           (authenticated ? (
-            <button className="w-20 p-2 mr-2 text-center text-white bg-gray-400 rounded">
+            <button
+              className="w-20 p-2 mr-2 text-center text-white bg-gray-400 rounded"
+              onClick={handleLogout}
+            >
               로그아웃
             </button>
           ) : (
