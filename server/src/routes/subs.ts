d8c81fbbd9c1c8fe497d027daf663210a42fb702
kimhsr <kimhsr@naver.com>
2023-03-07 21:07:21 +0900

    fix: axios 오류 수정

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,11 +1,11 @@
+import { isEmpty } from "class-validator";
 import { Router, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { User } from "../entities/User";
-import userMiddleware from "../middlewares/user";
-import authMiddleware from "../middlewares/auth";
-import { isEmpty } from "class-validator";
 import { AppDataSource } from "../data-source";
 import Sub from "../entities/Sub";
+import { User } from "../entities/User";
+import authMiddleware from "../middlewares/auth";
+import userMiddleware from "../middlewares/user";
 
 const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
@@ -13,19 +13,20 @@ const createSub = async (req: Request, res: Response, next) => {
   try {
     let errors: any = {};
     if (isEmpty(name)) errors.name = "이름은 비워둘 수 없습니다.";
-    if (isEmpty(title)) errors.title = "제목은 비워둘 수 없습니다.";
+    if (isEmpty(title)) errors.title = "제목은 비워두 수 없습니다.";
+
     const sub = await AppDataSource.getRepository(Sub)
       .createQueryBuilder("sub")
       .where("lower(sub.name) = :name", { name: name.toLowerCase() })
       .getOne();
 
     if (sub) errors.name = "서브가 이미 존재합니다.";
-
     if (Object.keys(errors).length > 0) {
       throw errors;
     }
   } catch (error) {
-    return res.status(400).json(error);
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
   }
 
   try {
@@ -38,8 +39,6 @@ const createSub = async (req: Request, res: Response, next) => {
     sub.user = user;
 
     await sub.save();
-
-    // 저장한 정보 프론트엔드로 전달해주기
     return res.json(sub);
   } catch (error) {
     console.log(error);
