cfa43d7b90ffe5044b3afc8bcc9ac91f1a6e7430
Kim Hyun Sung <kimhsr@naver.com>
2023-03-08 03:06:53 +0900

    feat: 상세 페이지 UI (#21)

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -8,6 +8,17 @@ import { User } from "../entities/User";
 import authMiddleware from "../middlewares/auth";
 import userMiddleware from "../middlewares/user";
 
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+
+  try {
+    const sub = await Sub.findOneByOrFail({ name });
+    return res.json(sub);
+  } catch (error) {
+    return res.status(404).json({ error: "커뮤니티를 찾을 수 없습니다." });
+  }
+};
+
 const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
 
@@ -69,6 +80,7 @@ const topSubs = async (req: Request, res: Response) => {
 
 const router = Router();
 
+router.get("/:name", userMiddleware, getSub);
 router.post("/", userMiddleware, authMiddleware, createSub);
 router.get("/sub/topSubs", topSubs);
 
