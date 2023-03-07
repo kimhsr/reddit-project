7a33177ad9d17485be6057596004a9bb593e3986
kimhsr <kimhsr@naver.com>
2023-03-07 23:02:48 +0900

    feat: 커뮤니티 리스트 UI

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,14 +4,18 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import { Sub } from "../types";
 import useSWR from "swr";
+import Image from "next/image";
+import { NextPage } from "next";
+import { useAuthState } from "../context/auth";
 
-const index = () => {
+const Home: NextPage = () => {
+  const { authenticated } = useAuthState();
+
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data);
   };
   const address = "http://localhost:4000/api/subs/sub/topSubs";
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
-  console.log("topSubs", topSubs);
 
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
@@ -26,18 +30,45 @@ const index = () => {
           </div>
 
           {/* 커뮤니티 리스트 */}
-          <div></div>
-          <div className="w-full py-6 text-center">
-            <Link href="subs/create">
-              <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
-                커뮤니티 만들기
-              </a>
-            </Link>
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`}>
+                  <a>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                </Link>
+                <Link href={`/r/${sub.name}`}>
+                  <a className="ml-2 font-bold hover:cursor-pointer">
+                    /r/{sub.name}
+                  </a>
+                </Link>
+                <p className="ml-auto font-med">{sub.postCount}</p>
+              </div>
+            ))}
           </div>
+          {authenticated && (
+            <div className="w-full py-6 text-center">
+              <Link href="subs/create">
+                <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
+                  커뮤니티 만들기
+                </a>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default index;
+export default Home;
