57096c8fc87c59d485182f8205dfaeacab28a0cd
kimhsr <kimhsr@naver.com>
2023-03-10 16:35:38 +0900

    feat: 포스트 댓글 생성

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import Comment from "../entities/Comment";
 import Post from "../entities/Post";
 import Sub from "../entities/Sub";
 import authMiddleware from "../middlewares/auth";
@@ -49,8 +50,31 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
+const createPostComment = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  const body = req.body.body;
+  try {
+    const post = await Post.findOneByOrFail({ identifier, slug });
+    const comment = new Comment();
+    comment.body = body;
+    comment.user = res.locals.user;
+    comment.post = post;
+
+    if (res.locals.user) {
+      post.setUserVote(res.locals.user);
+    }
+
+    await comment.save();
+    return res.json(comment);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({ error: "게시물을 찾을 수 없습니다" });
+  }
+};
+
 const router = Router();
 router.get("/:identifier/:slug", userMiddleware, getPost);
 router.post("/", userMiddleware, authMiddleware, createPost);
+router.post("/:identifier/:slug/comments", userMiddleware, createPostComment);
 
 export default router;
