0e244761e44235af319b45713360631d5cf33b2b
kimhsr <kimhsr@naver.com>
2023-03-11 01:55:23 +0900

    feat: Vote 함수 생성 및 router.pathname

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,12 +1,16 @@
+import axios from "axios";
 import dayjs from "dayjs";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { useAuthState } from "../context/auth";
 import { Post } from "../types";
 
 interface PostCardProps {
   post: Post;
+  subMutate: () => void;
 }
 
 const PostCard = ({
@@ -24,14 +28,30 @@ const PostCard = ({
     username,
     sub,
   },
+  subMutate,
 }: PostCardProps) => {
+  const router = useRouter();
+  const isInSubPage = router.pathname === "/r/[sub]";
+  console.log("router.pathname", router.pathname);
+
+  const { authenticated } = useAuthState();
+  const vote = async (value: number) => {
+    if (!authenticated) router.push("/login");
+    if (value === userVote) value = 0;
+    try {
+      await axios.post("/votes", { identifier, slug, value });
+      subMutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex mb-4 bg-white rounded" id={identifier}>
       <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
         {/* 좋아요 */}
         <div
           className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-          // onClick={() => vote(1, comment)}
+          onClick={() => vote(1)}
         >
           {userVote === 1 ? (
             <FaArrowUp className="text-red-500" />
@@ -43,7 +63,7 @@ const PostCard = ({
         {/* 싫어요 */}
         <div
           className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
-          // onClick={() => vote(-1, comment)}
+          onClick={() => vote(-1)}
         >
           {userVote === -1 ? (
             <FaArrowDown className="text-blue-500" />
@@ -54,25 +74,27 @@ const PostCard = ({
       </div>
       {/* 포스트 데이터 부분 */}
       <div className="w-full p-2">
-        {/* <div className="flex items-center">
-          <Link href={`/r/${subName}`}>
-            <a>
-              <Image
-                src={sub?.imageUrl}
-                alt="sub"
-                className="rounded-full cursor-pointer"
-                width={12}
-                height={12}
-              />
-            </a>
-          </Link>
-          <Link href={`/r/${subName}`}>
-            <a className="ml-2 text-xs font-bold cursor-pointer hover:underline">
-              /r/{subName}
-            </a>
-          </Link>
-          <span className="mx-1 text-xs text-gray-400">•</span>
-        </div> */}
+        {!isInSubPage && (
+          <div className="flex items-center">
+            <Link href={`/r/${subName}`}>
+              <a>
+                <Image
+                  src={sub?.imageUrl}
+                  alt="sub"
+                  className="rounded-full cursor-pointer"
+                  width={12}
+                  height={12}
+                />
+              </a>
+            </Link>
+            <Link href={`/r/${subName}`}>
+              <a className="ml-2 text-xs font-bold cursor-pointer hover:underline">
+                /r/{subName}
+              </a>
+            </Link>
+            <span className="mx-1 text-xs text-gray-400">•</span>
+          </div>
+        )}
 
         <p className="text-xs text-gray-400">
           Posted by
