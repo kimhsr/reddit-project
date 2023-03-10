8c0ebd5f5133da65c9639a402acf372cf5eb2f1f
Kim Hyun Sung <kimhsr@naver.com>
2023-03-10 22:01:50 +0900

    feat: 아이콘 변경 에러 해결 (#35)

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -7,6 +7,7 @@ import React, { FormEvent, useState } from "react";
 import useSWR from "swr";
 import { useAuthState } from "../../../../context/auth";
 import { Comment, Post } from "../../../../types";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 const PostPage = () => {
   const router = useRouter();
@@ -75,26 +76,26 @@ const PostPage = () => {
                 <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
                   {/* 좋아요 */}
                   <div
-                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
                     onClick={() => vote(1)}
                   >
-                    <i
-                      className={classNames("fas fa-arrow-up", {
-                        "text-red-500": post.userVote === 1,
-                      })}
-                    ></i>
+                    {post.userVote === 1 ? (
+                      <FaArrowUp className="text-red-500" />
+                    ) : (
+                      <FaArrowUp />
+                    )}
                   </div>
                   <p className="text-xs font-bold">{post.voteScore}</p>
                   {/* 싫어요 */}
                   <div
-                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                    className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
                     onClick={() => vote(-1)}
                   >
-                    <i
-                      className={classNames("fas fa-arrow-down", {
-                        "text-blue-500": post.userVote === -1,
-                      })}
-                    ></i>
+                    {post.userVote === -1 ? (
+                      <FaArrowDown className="text-blue-500" />
+                    ) : (
+                      <FaArrowDown />
+                    )}
                   </div>
                 </div>
                 <div className="py-2 pr-2">
@@ -174,26 +175,26 @@ const PostPage = () => {
                   <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
                     {/* 좋아요 */}
                     <div
-                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                      className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
                       onClick={() => vote(1, comment)}
                     >
-                      <i
-                        className={classNames("fas fa-arrow-up", {
-                          "text-red-500": comment.userVote === 1,
-                        })}
-                      ></i>
+                      {comment.userVote === 1 ? (
+                        <FaArrowUp className="text-red-500" />
+                      ) : (
+                        <FaArrowUp />
+                      )}
                     </div>
                     <p className="text-xs font-bold">{comment.voteScore}</p>
                     {/* 싫어요 */}
                     <div
-                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                      className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
                       onClick={() => vote(-1, comment)}
                     >
-                      <i
-                        className={classNames("fas fa-arrow-down", {
-                          "text-blue-500": comment.userVote === -1,
-                        })}
-                      ></i>
+                      {comment.userVote === -1 ? (
+                        <FaArrowDown className="text-blue-500" />
+                      ) : (
+                        <FaArrowDown />
+                      )}
                     </div>
                   </div>
                   <div className="py-2 pr-2">
