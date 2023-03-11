59d308891e412003f69eabb63845fbd19d8e72bd
kimhsr <kimhsr@naver.com>
2023-03-11 13:54:11 +0900

    feat: 포스트 나열

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { NextPage } from "next";
 import { useAuthState } from "../context/auth";
 import useSWRInfinite from "swr/infinite";
+import PostCard from "../components/PostCard";
 
 const Home: NextPage = () => {
   const { authenticated } = useAuthState();
@@ -33,13 +34,22 @@ const Home: NextPage = () => {
     isValidating,
     mutate,
   } = useSWRInfinite<Post[]>(getKey);
+  const isInitialLoading = !data && !error;
+  const posts: Post[] = data ? ([] as Post[]).concat(...data) : [];
 
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
 
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
       {/* 포스트 리스트 */}
-      <div className="w-full md:mr-3 md:w-8/12"></div>
+      <div className="w-full md:mr-3 md:w-8/12">
+        {isInitialLoading && (
+          <p className="text-lg text-center">로딩중입니다...</p>
+        )}
+        {posts?.map((post) => (
+          <PostCard key={post.identifier} post={post} />
+        ))}
+      </div>
 
       {/* 사이드바 */}
       <div className="hidden w-4/12 ml-3 md:block">
