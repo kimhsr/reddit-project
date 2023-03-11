b15852668e91f1159498056ccc7729c5c6340bc8
Kim Hyun Sung <kimhsr@naver.com>
2023-03-11 15:13:28 +0900

    feat: 유저 페이지 생성 (#41)

---
+++ client/src/pages/u/[username].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import React from "react";
+import useSWR from "swr";
+
+const UserPage = () => {
+  const router = useRouter();
+  const username = router.query.username;
+
+  const { data, error } = useSWR(username ? `/user/${username}` : null);
+  return <div>UserPage</div>;
+};
+
+export default UserPage;
